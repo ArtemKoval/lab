407982e219b9456e42a0383bb043282f35fe0a83
Art Koval <mail@artemkoval.com>
2025-03-01 21:22:52 +0300

    Start setting up movement attribute

---
+++ unity/art_survivors/Assets/Survivors/Scripts/Movement/MovementAttribute.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+namespace Survivors.Scripts.Movement {
+	[CreateAssetMenu(fileName = "MovementAttribute", menuName = "_Survivors/Scriptable Objects/MovementAttribute")]
+	public class MovementAttribute : ScriptableObject {
+		
+	}
+}
