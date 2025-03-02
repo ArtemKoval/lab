4438616feee2d848010786f402e3aca425e21f53
Art Koval <mail@artemkoval.com>
2025-03-02 11:18:23 +0300

    -- Reset project to blank universal 3d

--- unity/art_survivors/Assets/Survivors/Scripts/Movement/MovementAttribute.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-namespace Survivors.Scripts.Movement {
-	[CreateAssetMenu(fileName = "MovementAttribute", menuName = "_Survivors/Scriptable Objects/MovementAttribute")]
-	public class MovementAttribute : ScriptableObject {
-		
-	}
-}
--- unity/art_survivors/README.md
+++
@@ -1,7 +0,0 @@
-# Assets Used
-
-- https://assetstore.unity.com/packages/2d/undead-survivor-assets-pack-238068
-
-- https://github.com/DanielEverland/ScriptableObject-Architecture
-
-- https://vcontainer.hadashikick.jp/
