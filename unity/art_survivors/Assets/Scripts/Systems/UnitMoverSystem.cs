a4920ff7ffc150ce5a338eabb0e96eb6c769a516
Art Koval <mail@artemkoval.com>
2025-03-03 06:57:25 +0300

    Move with job

--- unity/art_survivors/Assets/Scripts/Systems/UnitMoverSystem.cs
+++ unity/art_survivors/Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -9,23 +9,52 @@ namespace Systems {
 	public partial struct UnitMoverSystem : ISystem {
 		[BurstCompile]
 		public void OnUpdate(ref SystemState state) {
-			foreach (var (localTransform,
-				         unitMover,
-				         physicsVelocity)
-			         in SystemAPI.Query<
-					         RefRW<LocalTransform>,
-					         RefRO<UnitMover>,
-					         RefRW<PhysicsVelocity>>
-				         ()) {
-				var moveDirection = unitMover.ValueRO.TargetPosition - localTransform.ValueRO.Position;
-				moveDirection = math.normalizesafe(moveDirection);
+			var unitMoverJob = new UnitMoverJob() {
+				deltaTime = SystemAPI.Time.DeltaTime
+			};
+			unitMoverJob.ScheduleParallel();
 
-				// for 2d use sprite flip instead of rotation todo
-				// localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
-
-				physicsVelocity.ValueRW.Linear = moveDirection * unitMover.ValueRO.MoveSpeed;
-				physicsVelocity.ValueRW.Angular = float3.zero; // freeze rotation on collisions
-			}
+			// foreach (var (localTransform,
+			// 	         unitMover,
+			// 	         physicsVelocity)
+			//          in SystemAPI.Query<
+			// 		         RefRW<LocalTransform>,
+			// 		         RefRO<UnitMover>,
+			// 		         RefRW<PhysicsVelocity>>
+			// 	         ()) {
+			// 	var moveDirection = unitMover.ValueRO.TargetPosition - localTransform.ValueRO.Position;
+			// 	moveDirection = math.normalizesafe(moveDirection);
+			//
+			// 	// for 2d use sprite flip instead of rotation todo
+			// 	// localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
+			//
+			// 	physicsVelocity.ValueRW.Linear = moveDirection * unitMover.ValueRO.MoveSpeed;
+			// 	physicsVelocity.ValueRW.Angular = float3.zero; // freeze rotation on collisions
+			// }
 		}
+	}
+}
+
+[BurstCompile]
+public partial struct UnitMoverJob : IJobEntity {
+	public float deltaTime;
+
+	public void Execute(
+		ref LocalTransform localTransform,
+		in UnitMover unitMover,
+		ref PhysicsVelocity physicsVelocity
+	) {
+		var moveDirection = unitMover.TargetPosition - localTransform.Position;
+		const float reachedTargetDistanceSq = 0.01f;
+		if (math.lengthsq(moveDirection) < reachedTargetDistanceSq) {
+			physicsVelocity.Linear = float3.zero;
+			physicsVelocity.Angular = float3.zero;
+			return;
+		}
+		moveDirection = math.normalizesafe(moveDirection);
+		// for 2d use sprite flip instead of rotation todo
+		// localTransform.ValueRW.Rotation = quaternion.LookRotation(moveDirection, math.up());
+		physicsVelocity.Linear = moveDirection * unitMover.MoveSpeed;
+		physicsVelocity.Angular = float3.zero; // freeze rotation on collisions
 	}
 }
