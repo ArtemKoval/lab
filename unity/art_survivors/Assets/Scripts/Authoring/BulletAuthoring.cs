27fdf6cdfbd0da0cd4b9b1cc65e1d8d570f29609
Art Koval <mail@artemkoval.com>
2025-03-10 17:29:52 +0300

    Add bullet

---
+++ unity/art_survivors/Assets/Scripts/Authoring/BulletAuthoring.cs
@@ -0,0 +1,23 @@
+using Unity.Entities;
+using UnityEngine;
+
+namespace Authoring {
+	public class BulletAuthoring : MonoBehaviour {
+		public float speed;
+		public float damageAmount;
+		public class Baker : Baker<BulletAuthoring> {
+			public override void Bake(BulletAuthoring authoring) {
+				var entity = GetEntity(TransformUsageFlags.Dynamic);
+				AddComponent(entity, new Bullet {
+					Speed = authoring.speed,
+					DamageAmount = authoring.damageAmount
+				});
+			}
+		}
+	}
+
+	public struct Bullet : IComponentData {
+		public float Speed;
+		public float DamageAmount;
+	}
+}
---
+++ unity/art_survivors/Assets/Scripts/Systems/BulletMoverSystem.cs
@@ -0,0 +1,38 @@
+using Authoring;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Systems {
+	partial struct BulletMoverSystem : ISystem {
+		[BurstCompile]
+		public void OnUpdate(ref SystemState state) {
+			var entityCommandBuffer = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
+				.CreateCommandBuffer(state.WorldUnmanaged);
+
+
+			foreach (var (localTransform, bullet, target, entity)
+			         in SystemAPI.Query<RefRW<LocalTransform>, RefRO<Bullet>, RefRO<Target>>().WithEntityAccess()) {
+				var targetLocalTransform =
+					SystemAPI.GetComponentRO<LocalTransform>(target.ValueRO.TargetEntity);
+				var distanceBeforeSq = math.distancesq(targetLocalTransform.ValueRO.Position, localTransform.ValueRO.Position);
+				var direction = targetLocalTransform.ValueRO.Position
+				                - localTransform.ValueRO.Position;
+				direction = math.normalize(direction);
+				localTransform.ValueRW.Position += direction * bullet.ValueRO.Speed * SystemAPI.Time.DeltaTime;
+				var distanceAfterSq = math.distancesq(targetLocalTransform.ValueRO.Position, localTransform.ValueRO.Position);
+				if (distanceAfterSq > distanceBeforeSq) {
+					localTransform.ValueRW.Position = targetLocalTransform.ValueRO.Position;
+				}
+
+				const float destroyDistanceSq = .2f;
+				if (!(math.distancesq(targetLocalTransform.ValueRO.Position, localTransform.ValueRO.Position) <
+				      destroyDistanceSq)) continue;
+				var targetHealth = SystemAPI.GetComponentRW<Health>(target.ValueRO.TargetEntity);
+				targetHealth.ValueRW.Value -= bullet.ValueRO.DamageAmount;
+				entityCommandBuffer.DestroyEntity(entity);
+			}
+		}
+	}
+}
